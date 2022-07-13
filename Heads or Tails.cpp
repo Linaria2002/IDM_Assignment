5b7b6f054a76193825b5488de709ccfe5e980b48
Linaria2002 <s1290128@u-aizu.ac.jp>
2022-07-13 22:09:47 +0900

    HoT initial revision

---
+++ Heads or Tails.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<ctime>
+using namespace std;
+
+int main(){
+    srand(time(nullptr));
+    
+    const int ROOP = 3;
+    int Heads = 0, Tails = 0;
+
+    cout << "Tossing a coin..." << endl;
+
+    for(int i = 1; i <= ROOP; i++){
+        cout << "Round " << i << ": ";
+        if(rand() & 1){
+            cout << "Heads" << endl;
+            Heads++;
+        }
+        else{
+            cout << "Tails" << endl;
+            Tails++;
+        }
+    }
+
+    cout << "Heads: " << Heads << ", Tails: " << Tails << endl;
+    return 0;
+}
